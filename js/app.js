683c73cb12d0fe2c476f679cb66bf275df0ecf48
pio-kniaz <pio.kniaz@gmail.com>
2017-08-22 18:12:46 +0200

    added fontello and new nav icon

--- js/app.js
+++ js/app.js
@@ -1,14 +1,1 @@
 $(document).foundation();
-// (function(){
-//   const button=document.querySelector(".submitButton");
-//
-//   button.addEventListener("click",function(event){
-//       event.preventDefault();
-//       const inputVal = document.querySelector(".inputPassword").value;
-//
-//       console.log(inputVal);
-//       location.replace("index.html")
-//
-//   });
-//
-// })();
