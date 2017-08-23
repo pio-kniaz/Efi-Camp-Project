1290dfec9691caca223b578206a86bd4988edf89
pio-kniaz <pio.kniaz@gmail.com>
2017-08-23 16:18:21 +0200

    Balance Api added

---
+++ js/balance.js
@@ -0,0 +1,14 @@
+$.ajax({
+  method:'get',
+  url:'https://efigence-camp.herokuapp.com/api/data/summary',
+  dataTypes:'jeson',
+  success:function(data){
+    console.log(data);
+    // $.map(data,function(moni,i){
+      $('#balance-api').append(data.content[0].balance);
+      $('#funds-api').append(data.content[0].funds);
+      $('#payments-api').append(data.content[0].payments);
+    // })
+  }
+
+})
